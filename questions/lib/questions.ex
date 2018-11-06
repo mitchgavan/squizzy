f16d404d7240458a2307469b098d3ab915af28ed
Mitch Gavan <mitchgavan@gmail.com>
2018-11-07 07:15:18 +1100

    cleanup questions

--- questions/lib/questions.ex
+++ questions/lib/questions.ex
@@ -1,23 +1,15 @@
 defmodule Questions do
   
-  def question_list do
-    "../assets/questions.txt"
-    |> Path.expand(__DIR__)
-    |> File.read!()
-    |> String.split(~r/\n/)
-    |> Enum.reject(fn(v) -> v == "" end) 
-  end
-
-  def random_question do
-    get_questions()
-    |> Enum.random()
-  end
-
   def get_questions do
     "../assets/quiz.json"
     |> Path.expand(__DIR__)
     |> File.read!()
     |> Poison.Parser.parse!()
+  end
+
+  def random_question do
+    get_questions()
+    |> Enum.random()
   end
 
 end
