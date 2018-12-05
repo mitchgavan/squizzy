9ffe300569654ea1014683594a982d9c42a009df
Mitch Gavan <mitchgavan@gmail.com>
2018-12-06 07:48:30 +1100

    add supervisor to dictionary

--- dictionary/lib/dictionary/application.ex
+++ dictionary/lib/dictionary/application.ex
@@ -4,7 +4,20 @@ defmodule Dictionary.Application do
 
   # use start instead of start_link so this doesn't take down the entire elixir world when it crashes. This is the root process of the application. The two args are rarely used.
   def start(_type, _args) do
-    Dictionary.QuestionList.start_link()
+
+    import Supervisor.Spec
+
+    children = [
+      worker(Dictionary.QuestionList, []),
+    ]
+
+    # strategy defines how to restart processes
+    options = [
+      name: Dictionary.Supervisor,
+      strategy: :one_for_one,
+    ]
+
+    Supervisor.start_link(children, options)
   end
   
 end
