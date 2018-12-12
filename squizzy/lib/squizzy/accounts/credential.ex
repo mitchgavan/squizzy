62960b2463fbfbef33c4a239b6873c86730727d2
Mitch Gavan <mitchgavan@gmail.com>
2018-12-13 08:00:25 +1100

    feat: associate credentials with user

--- squizzy/lib/squizzy/accounts/credential.ex
+++ squizzy/lib/squizzy/accounts/credential.ex
@@ -4,9 +4,10 @@ defmodule Squizzy.Accounts.Credential do
 
 
   schema "credentials" do
-    field :" email", :string
+    field :email, :string
+    field :password, :string, virtual: true
     field :password_hash, :string
-    field :user_id, :id
+    belongs_to :user, Squizzy.Accounts.User
 
     timestamps()
   end
@@ -14,8 +15,20 @@ defmodule Squizzy.Accounts.Credential do
   @doc false
   def changeset(credential, attrs) do
     credential
-    |> cast(attrs, [:" email", :password_hash])
-    |> validate_required([:" email", :password_hash])
-    |> unique_constraint(:" email")
+    |> cast(attrs, [:email, :password])
+    |> validate_required([:email, :password])
+    |> validate_length(:password, min: 6, max: 100)
+    |> unique_constraint(:email)
+    |> put_pass_hash()
+  end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Comeonin.Pbkdf2.hashpwsalt(pass))
+      
+      _ ->
+        changeset
+    end
   end
 end
