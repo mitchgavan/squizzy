4205ab38e2fe482b67d98c00282a958a8edd2347
Mitch Gavan <mitchgavan@gmail.com>
2018-12-21 09:22:24 +1100

    feat: restrict edit and upload of videos

--- squizzy/lib/squizzy/multimedia/multimedia.ex
+++ squizzy/lib/squizzy/multimedia/multimedia.ex
@@ -19,7 +19,16 @@ defmodule Squizzy.Multimedia do
 
   """
   def list_videos do
-    Repo.all(Video)
+    Video
+    |> Repo.all(Video)
+    |> preload_user()
+  end
+
+  def list_user_videos(%Accounts.User{} = user) do
+    Video
+    |> user_videos_query(user)
+    |> Repo.all()
+    |> preload_user()
   end
 
   @doc """
@@ -36,7 +45,14 @@ defmodule Squizzy.Multimedia do
       ** (Ecto.NoResultsError)
 
   """
-  def get_video!(id), do: Repo.get!(Video, id)
+  def get_video!(id), do: preload_user(Repo.get!(Video, id))
+
+  def get_user_video!(%Accounts.User{} = user, id) do
+    from(v in Video, where: v.id == ^id)
+    |> user_videos_query(user)
+    |> Repo.one!()
+    |> preload_user()
+  end
 
   @doc """
   Creates a video.
@@ -108,5 +124,13 @@ defmodule Squizzy.Multimedia do
 
   defp put_user(changeset, user) do
     Ecto.Changeset.put_assoc(changeset, :user, user)
+  end
+
+  defp user_videos_query(query, %Accounts.User{id: user_id}) do
+    from(v in query, where: v.user_id == ^user_id)
+  end
+
+  defp preload_user(video_or_videos) do
+    Repo.preload(video_or_videos, :user)
   end
 end
