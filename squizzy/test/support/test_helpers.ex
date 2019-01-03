05db0e8278960341428ab03bf16ea8e5ed39fcca
Mitch Gavan <mitchgavan@gmail.com>
2019-01-04 07:51:33 +1100

    add test helpers

---
+++ squizzy/test/support/test_helpers.ex
@@ -0,0 +1,37 @@
+defmodule Squizzy.TestHelpers do
+  
+  alias Squizzy.{
+    Accounts,
+    Multimedia
+  }
+
+  def user_fixture(attrs \\ %{}) do
+    username = "user#{System.unique_integer([:positive])}"
+
+    {:ok, user} =
+      attrs
+      |> Enum.into(%{
+        name: "Some user",
+        username: username,
+        credential: %{
+          email: attrs[:email] || "#{username}@example.com",
+          password: attrs[:password] || "supersecret",
+        }
+      })
+      |> Accounts.register_user()
+
+    user
+  end
+
+  def video_fixture(%Accounts.User{} = user, attrs \\ ${}) do
+    attrs = Enum.into(attrs, %{
+      title: "A Title",
+      url: "http://google.com",
+      description: "a description"
+    })
+
+    {:ok, video} = Multimedia.create_video(user, attrs)
+
+    video
+  end
+end
