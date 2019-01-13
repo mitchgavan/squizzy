948513e84a566e085876d6e5b85783089fd4308a
Mitch Gavan <mitchgavan@gmail.com>
2019-01-14 07:35:20 +1100

    test user auth on video routes

---
+++ squizzy/test/squizzy_web/controllers/video_controller_test.exs
@@ -0,0 +1,39 @@
+defmodule SquizzyWeb.VideoControllerTest do
+  use SquizzyWeb.ConnCase
+
+  test "requires user authentication on all actions", %{conn: conn} do
+    Enum.each([
+      get(conn, Routes.video_path(conn, :new)),
+      get(conn, Routes.video_path(conn, :index)),
+      get(conn, Routes.video_path(conn, :show, "123")),
+      get(conn, Routes.video_path(conn, :edit, "123")),
+      put(conn, Routes.video_path(conn, :update, "123", %{})),
+      post(conn, Routes.video_path(conn, :create, %{})),
+      delete(conn, Routes.video_path(conn, :delete, "123")),
+    ], fn conn ->
+      assert html_response(conn, 302)
+      assert conn.halted
+    end)
+  end
+
+  describe "with a logged in user" do
+    setup %{conn: conn, login_as: username} do
+      user = user_fixture(username: username)
+      conn = assign(conn, :current_user, user)
+  
+      {:ok, conn: conn, user: user}
+    end
+
+    @tag login_as: "max"
+
+    test "lists all user's video on index", %{conn: conn, user: user} do
+      user_video = video_fixture(user, title: "funny cats")
+      other_video = video_fixture(user_fixture(username: "other"), title: "another video")
+      conn = get conn, Routes.video_path(conn, :index)
+
+      assert html_response(conn, 200) =~ ~r/Listing Videos/
+      assert String.contains?(conn.resp_body, user_video.title)
+      refute String.contains?(conn.resp_body, other_video.title)
+    end
+  end
+end
--- squizzy/test/support/conn_case.ex
+++ squizzy/test/support/conn_case.ex
@@ -19,6 +19,9 @@ defmodule SquizzyWeb.ConnCase do
     quote do
       # Import conveniences for testing with connections
       use Phoenix.ConnTest
+
+      import Squizzy.TestHelpers
+      
       alias SquizzyWeb.Router.Helpers, as: Routes
 
       # The default endpoint for testing
